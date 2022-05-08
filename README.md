2cd20a76179dd0bfc40a52eb82b727b16fbeda9d
angelospart1 <elkapro19@gmail.com>
2022-05-08 13:57:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# https-typescript-fsh5hr-stackblitz-io
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/https-typescript-fsh5hr-stackblitz-io)
---
+++ index.ts
@@ -0,0 +1,17 @@
+console.log('hello all ');
+console.log(' this is a calculatrice (just + )');
+let n1, n2, n3: number;
+n1 = parseInt(prompt('taper le 1er nomber '));
+n2 = parseInt(prompt('taper le 2eme nomber '));
+n3 = n1 + n2;
+console.log('la solution est :');
+console.log(n3);
+if (n3 > 0) {
+  console.log(' cette nombre est positif ');
+}
+if (n3 < 0) {
+  console.log('cette nombre est negatif');
+}
+if ((n3 = 0)) {
+  console.log('le nombre null');
+}
